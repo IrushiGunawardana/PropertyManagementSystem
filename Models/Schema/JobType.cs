ebeff8ffd2615b5bcbc41484aa851adcce2a8d79
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 13:30:51 +0530

    Create jobtype model

---
+++ Models/Schema/JobType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PropertyManagementSystem.Models.Schema
+{
+    public class JobType
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ServiceProviderJobType> ServiceProviderJobTypes { get; set; }
+        public ICollection<Job> Jobs { get; set; }
+    }
+}
