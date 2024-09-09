c6ff92df337696c962b760081f489841fae29310
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 13:30:18 +0530

    Create property model

---
+++ Models/Schema/Property.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PropertyManagementSystem.Models.Schema
+{
+    public class Property
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Address { get; set; }
+
+        public ICollection<PropertyManager> PropertyManagers { get; set; }
+        public ICollection<PropertyOwner> PropertyOwners { get; set; }
+        public ICollection<PropertyTenant> PropertyTenants { get; set; }
+        public ICollection<Job> Jobs { get; set; }
+
+
+    }
+
+}
+
