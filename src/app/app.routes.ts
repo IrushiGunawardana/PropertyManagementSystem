a9cc30d2182bf00f1fc09b23321320f69f9bf083
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 12:18:27 +0530

    Create routes for the navigation

---
+++ src/app/app.routes.ts
@@ -0,0 +1,41 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { JobPostWizardsJobDetailsComponent } from './components/job-post-wizards-job-details/job-post-wizards-job-details.component';
+import { JobPostWizardsServiceProviderComponent } from './components/job-post-wizards-service-provider/job-post-wizards-service-provider.component';
+import { JobDetailsComponent } from './components/job-details/job-details.component';
+import { RegisterComponent } from './components/user-register/user-register.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component:DashboardComponent,
+      },
+      {
+        path: 'login',
+        component: UserLoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'jobpostdetails',
+        component:JobPostWizardsJobDetailsComponent,
+      },
+      {
+        path: 'serviceproviderdetails',
+        component:JobPostWizardsServiceProviderComponent,
+      },
+      {
+        path: 'jobdetails/:id',
+        component:JobDetailsComponent,
+      },
+
+];
