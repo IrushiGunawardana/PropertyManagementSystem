b983559569833f675584db2dc7f8203afc4b5003
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 13:22:33 +0530

    Create User model

---
+++ Models/Schema/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PropertyManagementSystem.Models.Schema
+{
+    public class User : IdentityUser<Guid>
+    {
+
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public ICollection<PropertyManager> PropertyManagers { get; set; }
+        public ICollection<PropertyOwner> PropertyOwners { get; set; }
+        public ICollection<PropertyTenant> PropertyTenants { get; set; }
+        public ICollection<ServiceProvider> ServiceProviders { get; set; }
+        public ICollection<Job> PostedJobs { get; set; }
+
+
+    }
+}
+
