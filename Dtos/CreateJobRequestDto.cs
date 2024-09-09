b389f6d5b3ae64c2d32bc4c993dded902d43287d
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 14:02:34 +0530

    Create job dto

---
+++ Dtos/CreateJobRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PropertyManagementSystem.Dtos
+{
+    public class CreateJobRequestDto
+    {
+        public Guid PropertyId {  get; set; }
+        public string Description { get; set; }
+
+        public Guid Type { get; set; }
+
+        public Guid ServiceProviderId { get; set; }
+
+    }
+}
---
+++ Dtos/SingleJobDto.cs
@@ -0,0 +1,61 @@
+﻿namespace PropertyManagementSystem.Dtos
+{
+    public class SingleJobDto
+    {
+        public Guid Id { get; set; }
+
+        public SinglePropertyDtoResponse property { get; set; }
+        public List<SingleOwnerDtoResponse> OwnerDetails { get; set; }
+
+        public List<SingleTenantDtoResponse> TenantDetails { get; set; }
+
+        public string Description { get; set; }
+        public int JobNumber { get; set; }
+
+        public JobTypeDtoResponse JobType { get; set; }
+
+        public ProviderDtoResponse Provider { get; set; }
+
+
+    }
+
+    public class SinglePropertyDtoResponse
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+
+    }
+    public class SingleOwnerDtoResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class SingleTenantDtoResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class JobTypeDtoResponse
+    {
+        public Guid Id { get; set; }
+        public string Name{ get; set; }
+
+    }
+
+    public class ProviderDtoResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
