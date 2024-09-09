e3523f128095ffb8dd544968b82e1655b7698e34
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 13:55:14 +0530

    Create user service

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PropertyManagementSystem.Dtos;
+
+namespace PropertyManagementSystem.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IdentityResult> RegisterAsync(RegisterRequestDto model);
+        Task<string> LoginAsync(LoginRequestDto model);
+    }
+}
