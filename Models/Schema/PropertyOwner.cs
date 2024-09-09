1fb6a8c309f87d756e06a142585175722916d1cc
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-09-09 13:29:20 +0530

    Create property owner model

---
+++ Models/Schema/PropertyOwner.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropertyManagementSystem.Models.Schema
+{
+    public class PropertyOwner
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid PropertyId { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [ForeignKey("PropertyId")]
+        public Property Property { get; set; }
+
+
+
+    }
+}
